ab5d72b4ee4a889e7c6b4d890289f71e039b846a
daisuke kato <bonukunpg@gmail.com>
2023-10-02 01:16:51 +0900

    途中save

--- Nuxt/pages/index.vue
+++ Nuxt/pages/index.vue
@@ -1,25 +1,35 @@
 <template>
   <v-container>
-    <p>参考文献リストの書式を選択</p>
+    <h1>書誌情報つくる君</h1>
     <v-select
-      label="Select"
-      :items="[
-        'California',
-        'Colorado',
-        'Florida',
-        'Georgia',
-        'Texas',
-        'Wyoming',
-      ]"
+      v-model="default_style"
+      label="参考文献リストの書式を選択"
+      :items="['日本心理学会執筆・投稿の手びき_2015年改訂版']"
+      variant="underlined"
+      return-object
     ></v-select>
-    <v-btn>aaa</v-btn>
+    <v-select
+      label="参考文献の形態を選択"
+      :items="['雑誌論文', '図書', 'ウェブサイト']"
+      variant="underlined"
+    ></v-select>
+    <v-textarea label="BibTeXペースト欄"></v-textarea>
+    <v-btn @click="handleButtonClick">書式事項を抽出</v-btn>
+    <v-text-field
+      v-model="authorNum"
+      label="著者の人数を入力"
+      type="number"
+      max="20"
+    ></v-text-field>
   </v-container>
 </template>
 
-<style>
-select {
-  -moz-appearance: menulist-button;
-  -webkit-appearance: menulist-button;
-  appearance: menulist-button;
-}
-</style>
+<script setup>
+const authorNum = ref();
+const default_style = "日本心理学会執筆・投稿の手びき_2015年改訂版";
+const handleButtonClick = () => {
+  console.log(authorNum.value);
+};
+</script>
+
+<style></style>
