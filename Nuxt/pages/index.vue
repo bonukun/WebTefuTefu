5f6d8acf20a97584e75c04092fdafaf81bc15005
bonukun <95573462+bonukun@users.noreply.github.com>
2023-10-03 21:32:03 +0900

    著者の入力欄だけ作成しました。 (#5)
    * 矢印が表示されるまで。
    
    * 途中save
    
    * 著者入力欄の作成
    
    * 著者入力欄だけ作成

--- Nuxt/pages/index.vue
+++ Nuxt/pages/index.vue
@@ -1,35 +1,80 @@
 <template>
-  <h1>ここからシステムのようなものを作成していきますよ。</h1>
-  <v-app>
-    <v-container>
-      <v-card
-        width="400"
-        title="This is a title"
-        subtitle="This is a subtitle"
-        text="This is content"
-      ></v-card>
-
-      <div class="mt-4 text-subtitle-2">With slots</div>
-
-      <v-card width="400">
-        <template v-slot:title> This is a title </template>
-
-        <template v-slot:subtitle> This is a subtitle </template>
-
-        <template v-slot:text> This is content </template>
-      </v-card>
-
-      <div class="mt-4 text-subtitle-2">With markup</div>
-
-      <v-card width="400">
-        <v-card-item>
-          <v-card-title>This is a title</v-card-title>
-
-          <v-card-subtitle>This is a subtitle</v-card-subtitle>
-        </v-card-item>
-
-        <v-card-text> This is content </v-card-text>
-      </v-card>
-    </v-container>
-  </v-app>
+  <v-container>
+    <h1>書誌情報つくる君</h1>
+    <v-select
+      v-model="default_style"
+      label="参考文献リストの書式を選択"
+      :items="['日本心理学会執筆・投稿の手びき_2015年改訂版']"
+      variant="underlined"
+      return-object
+    ></v-select>
+    <v-select
+      label="参考文献の形態を選択"
+      :items="['雑誌論文', '図書', 'ウェブサイト']"
+      variant="underlined"
+    ></v-select>
+    <v-textarea label="BibTeXペースト欄"></v-textarea>
+    <v-btn
+      @click="handleButtonClick"
+      class="text-capitalize"
+      style="margin-bottom: 16px"
+      >BibTeXから書式事項を抽出</v-btn
+    >
+    <v-row>
+      <v-col>
+        <v-text-field
+          v-model="authorNum"
+          label="著者の人数を入力"
+          type="number"
+          @input="authorNum_limit"
+          max="20"
+          min="1"
+        ></v-text-field>
+      </v-col>
+      <v-col>
+        <v-btn icon color="red" style="margin-right: 16px" @click="author_add">
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+        <v-btn icon color="blue" @click="author_remove">
+          <v-icon>mdi-minus</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row v-for="n in parseInt(authorNum.toString())" :key="n">
+      <v-text-field :label="`第${n}著者名を入力`" variant="underlined">
+      </v-text-field>
+    </v-row>
+  </v-container>
 </template>
+
+<script setup lang="ts">
+const default_style = "日本心理学会執筆・投稿の手びき_2015年改訂版";
+
+const handleButtonClick = () => {
+  console.log(authorNum.valueOf);
+};
+
+const authorNum = ref(1);
+
+const authorNum_limit = () => {
+  console.log(authorNum.value);
+  if (authorNum.value > 20) {
+    authorNum.value = 20;
+  }
+};
+
+const author_add = () => {
+  if (authorNum.value == 20) {
+    return;
+  }
+  authorNum.value++;
+};
+const author_remove = () => {
+  if (authorNum.value == 1) {
+    return;
+  }
+  authorNum.value--;
+};
+</script>
+
+<style></style>
