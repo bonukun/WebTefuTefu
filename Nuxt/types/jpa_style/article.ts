e6c433bd206d98c34b17ad4cd3a931642c0c3ab3
daisuke kato <bonukunpg@gmail.com>
2023-10-13 21:28:16 +0900

    型の追加など

--- Nuxt/types/jpa_style/article.ts
+++ Nuxt/types/jpa_style/article.ts
@@ -1,23 +1,26 @@
-export class Article {
+import { base_ref } from "./base";
+
+export class Article extends base_ref {
   //著者名，刊行年，表題，誌名，巻数，ページ
   //中沢 潤・国本 小百合・祐宗 省三（1978）．幼児の弁別学習──非次元性課題における過剰訓練効果── 心理学研究，49，131-136
-  author: string;
-  year: string;
-  title: string;
-  jounal: string;
-  volume: string;
-  pages: string;
+  // author: string;
+  // year: string;
+  // title: string;
+  // jounal: string;
+  // volume: string;
+  // pages: string;
 
   constructor() {
+    super();
     this.author = "";
     this.year = "";
-    this.title = "たぬきち";
-    this.jounal = "";
+    this.title = "";
+    this.journal = "";
     this.volume = "";
     this.pages = "";
   }
 
   to_list = () => {
-    return ["author", "year", "title", "jounal", "volume", "pages"];
+    return ["year", "title", "jounal", "volume", "pages"];
   };
 }
