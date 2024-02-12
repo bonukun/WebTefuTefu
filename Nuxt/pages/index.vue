2e561bc12675b4101e117bec4aeacdaefe6261ba
Daisuke <bonukunpg@gmail.com>
2024-02-12 22:04:52 +0900

    APIレスポンスを表示できるようにした。

--- Nuxt/pages/index.vue
+++ Nuxt/pages/index.vue
@@ -1,24 +1,24 @@
 <template>
     <h1>書誌情報生成する君</h1>
     <button @click="addTask">test</button>
-    <p>{{ count }}</p>
+    <p>{{datas}}</p>
 
 </template>
 
 <script setup lang="ts">
-const url = "内緒";
+const url = "秘密";
 
-let count = ref(0);
-const countUp = (): number => {
-  return count.value++;
-};
+const datas =ref()
 
-const addTask = () => {
-  const { data } = useFetch(url, {
+
+const addTask = async () => {
+  const { data } = await useFetch(url, {
     method: 'get'
   });
+  datas.value=data.value;
   console.log("test")
   console.log(data)
 };
+
 
 </script>
