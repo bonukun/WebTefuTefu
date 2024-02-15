4a0b3313089cf7db112e0f4bea3fc45417529861
Daisuke <bonukunpg@gmail.com>
2024-02-15 22:37:10 +0900

    フロントエンドが応答を受け取れるようにした。

--- Nuxt/pages/index.vue
+++ Nuxt/pages/index.vue
@@ -8,15 +8,18 @@
     placeholder="BibTeXを入力してください"
   ></textarea>
   <br />
-  <button @click="addTask">送信</button>
-  <textarea v-moder="result" rows="3" cols="50" readonly></textarea>
+  <button @click="sendBibTexString">送信</button>
+  <br />
+  <textarea v-model="result" rows="3" cols="50" readonly>{{ result }}</textarea>
+  <br />
+  <button @click="copyClipBord">結果をコピーする</button>
+  <button @click="testApi">TestApi</button>
 </template>
 
 <script setup lang="ts">
 const runtimeConfig = useRuntimeConfig();
-const apiurl = runtimeConfig.public.apiUrlBase;
-const url = apiurl + "/Test";
-const text = ref();
+const apiUrl = runtimeConfig.public.apiDevelopUrlBase;
+const url = apiUrl + "/Test";
 const result = ref();
 const bibTeXString = ref();
 result.value = "ここに処理結果が表示されます";
@@ -24,14 +27,27 @@ result.value = "ここに処理結果が表示されます";
 const datas = ref();
 datas.value = "いえええい";
 
-const addTask = async () => {
+const sendBibTexString = async () => {
+  const url = apiUrl + "/BibTeX/postBibTeX";
+  console.log(url);
+  const { data } = await useFetch(url, {
+    method: "post",
+    body: { bibTeX: bibTeXString.value },
+  });
+  result.value = data.value.resultBibTeX;
+};
+
+const copyClipBord = () => {
+  console.log("クリップボードをコピーする関数処理が実行されました。");
+  navigator.clipboard.writeText("この中身がクリップボードに書き込まれます");
+};
+
+const testApi = async () => {
   const { data } = await useFetch(url, {
     method: "get",
   });
   console.log(url);
-  datas.value = data.value;
-  console.log("処理実行");
-  console.log(runtimeConfig.public.apiUrlBase);
-  console.log(process.env.NUXT_PUBLIC_API_URL);
+  data.value = data.value;
+  console.log(data);
 };
 </script>
