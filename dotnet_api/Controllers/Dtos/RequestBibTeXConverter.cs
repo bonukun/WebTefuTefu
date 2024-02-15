ccc25129c66ff9c5282d2747d347990d06522d61
Daisuke <bonukunpg@gmail.com>
2024-02-15 21:32:39 +0900

    バックエンドコントローラ部作成

---
+++ dotnet_api/Controllers/Dtos/RequestBibTeXConverter.cs
@@ -0,0 +1,11 @@
+namespace　WebTefufu.Controllers.Dtos{
+/// <summary>
+/// フロントエンドから送信されるリクエストの型を定義
+/// </summary>
+    public class RequestBibTeXConverter {
+        /// <summary>
+        /// BibTeX
+        /// </summary>
+        public string BibTeX {get; set;}
+    }
+}
---
+++ dotnet_api/Controllers/Dtos/ResponceBibTeXConverter.cs
@@ -0,0 +1,12 @@
+namespace　WebTefufu.Controllers.Dtos
+{
+/// <summary>
+/// フロントエンドから送信されるレスポンスの型を定義
+/// </summary>
+    public class ResponseBibTeXConverter {
+        /// <summary>
+        /// BibTeX
+        /// </summary>
+        public string ResultBibTeX {get; set;}
+    }
+}
