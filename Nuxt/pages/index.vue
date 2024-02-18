4da2e7e316000ba60607d2a6f75b0ad4abf5ed88
bonukun <95573462+bonukun@users.noreply.github.com>
2024-02-18 10:36:44 +0000

    2024 02 18/fix readme (#18)
    * md ファイルをちょっと書き直した。
    
    * ついでにフロントエンドを少し直した。

--- Nuxt/pages/index.vue
+++ Nuxt/pages/index.vue
@@ -9,6 +9,7 @@
   ></textarea>
   <br />
   <button @click="sendBibTexString">送信</button>
+  <p v-if="processing">処理中...</p>
   <br />
   <textarea v-model="result" rows="6" cols="80" readonly>{{ result }}</textarea>
   <br />
@@ -17,10 +18,12 @@
 
 <script setup lang="ts">
 const runtimeConfig = useRuntimeConfig();
-const apiUrl = runtimeConfig.public.apiDevelopUrlBase;
+const apiUrl = runtimeConfig.public.apiUrlBase;
 const url = apiUrl + "/Test";
 const result = ref();
 const bibTeXString = ref();
+const processing = ref();
+processing.value = false;
 result.value = "ここに処理結果が表示されます";
 
 const datas = ref();
@@ -28,12 +31,21 @@ datas.value = "";
 
 const sendBibTexString = async () => {
   const url = apiUrl + "/BibTeX/postBibTeX";
+  processing.value = true;
   console.log(url);
-  const { data } = await useFetch(url, {
-    method: "post",
-    body: { bibTeX: bibTeXString.value },
-  });
-  result.value = data.value.resultBibTeX;
+  try {
+    const { data } = await useFetch(url, {
+      method: "post",
+      body: { bibTeX: bibTeXString.value },
+    });
+    result.value = data.value.resultBibTeX;
+    processing.value = false;
+  } catch (e) {
+    console.log(e);
+    processing.value = false;
+    result.value =
+      "サーバーが応答を停止しているようです。後ほどお試しください。";
+  }
 };
 
 const copyClipBord = () => {
